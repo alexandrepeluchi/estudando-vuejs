6e51e4e427c75e722850300b68c9b7f3d784a122
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-07-30 14:51:27 -0300

    Removido a Vue HelloWorld para iniciar

--- estudando_vuejs/Vue CLI/school_app/src/App.vue
+++ estudando_vuejs/Vue CLI/school_app/src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Alunos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Alunos from './components/Alunos.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Alunos
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
