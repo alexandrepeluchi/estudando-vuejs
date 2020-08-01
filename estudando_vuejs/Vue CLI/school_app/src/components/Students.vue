e9cb57fb318681ec101b4bbadbbde12f0667a95c
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-07-31 21:03:18 -0300

    Alterado o nome da Vue 'Alunos' para 'Students'

---
+++ estudando_vuejs/Vue CLI/school_app/src/components/Students.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{name}}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Students",
+    }
+  },
+  props: {},
+};
+</script>
+
+<style scoped>
+</style>
