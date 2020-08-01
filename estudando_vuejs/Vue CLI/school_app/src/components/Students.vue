4cf83aec77c04732fcc1f409b620400140afd16e
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-08-01 12:59:29 -0300

    Adicionando 'student' em uma 'table'

--- estudando_vuejs/Vue CLI/school_app/src/components/Students.vue
+++ estudando_vuejs/Vue CLI/school_app/src/components/Students.vue
@@ -2,7 +2,6 @@
   <div>
     <h1>{{title}}</h1>
     <input type="text" placeholder="Student name" v-model="name" v-on:keyup.enter="addStudent()" />
-
     <table border="1px">
       <thead>
         <th>Registration</th>
@@ -10,9 +9,9 @@
         <th>Options</th>
       </thead>
       <tbody>
-        <tr>
-          <td>1</td>
-          <td>Jack</td>
+        <tr v-for="(student, index) in students" :key="index">
+          <td>{{index+1}}</td>
+          <td>{{student.name}}</td>
           <td>
             <button class="btn" @click="remove()">Remove</button>
           </td>
@@ -34,10 +33,11 @@ export default {
   props: {},
   methods: {
     addStudent() {
-      this.students.push(this.name);
-      this.students.forEach((element) => {
-        console.log(element);
-      });
+      let _student = {
+        name: this.name,
+      };
+      this.students.push(_student);
+      this.name = "";
     },
   },
 };
