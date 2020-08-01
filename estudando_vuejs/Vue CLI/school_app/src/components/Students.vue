b3d6c18a4c4046d5bdc0aa107c70e90d5149ad09
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2020-07-31 21:25:53 -0300

    Gerado uma simples table de alunos

--- estudando_vuejs/Vue CLI/school_app/src/components/Students.vue
+++ estudando_vuejs/Vue CLI/school_app/src/components/Students.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>{{name}}</div>
+  <div>
+    <h1>{{title}}</h1>
+    <input type="text" placeholder="Student name" v-model="name" />
+
+    <table border="1px">
+      <thead>
+        <th>Registration</th>
+        <th>Name</th>
+        <th>Options</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Jack</td>
+          <td>
+            <button class="btn" @click="remove()">Remove</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      title: "Student",
       name: "Students",
-    }
+    };
   },
   props: {},
 };
